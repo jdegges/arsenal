9b8e56d658426624343a016bfc2ee24f89a1d9ab
Joey Degges <jdegges@gmail.com>
2010-12-18 23:13:40 -0800

    Remove unsupported filesystem prototypes that modify the FS

--- src/sftp.h
+++ src/sftp.h
@@ -29,21 +29,6 @@ sftp_readlink (struct sftp *s, const char *path, char *buf, size_t bufsize);
 ssize_t
 sftp_realpath (struct sftp *s, const char *path, char *buf, size_t bufsize);
 
-int
-sftp_mkdir (struct sftp *s, const char *path, mode_t mode);
-
-int
-sftp_unlink (struct sftp *s, const char *path);
-
-int
-sftp_rmdir (struct sftp *s, const char *path);
-
-int
-sftp_symlink (struct sftp *s, const char *path1, const char *path2);
-
-int
-sftp_rename (struct sftp *s, const char *old, const char *new);
-
 struct sftp_fd *
 sftp_open (struct sftp *s, const char *path, int flags, mode_t mode);
 
@@ -52,9 +37,6 @@ sftp_close (struct sftp_fd * fd);
 
 int
 sftp_read (struct sftp_fd *fd, void *buf, size_t nbyte, off_t offset);
-
-int
-sftp_write (struct sftp_fd *fd, const void *buf, size_t nbyte);
 
 int
 sftp_statvfs (struct sftp *s, const char *path, struct statvfs *buf);
@@ -70,8 +52,5 @@ sftp_readdir (struct sftp_dir *dir);
 
 int
 sftp_closedir (struct sftp_dir *dir);
-
-struct sftp_fd *
-sftp_creat (struct sftp *s, const char *path, mode_t mode);
 
 #endif
